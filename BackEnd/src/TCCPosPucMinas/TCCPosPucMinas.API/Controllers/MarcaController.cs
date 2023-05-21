f6dd6ed104b32c64b80ac31690d62e93958e273b
Flavio <freelancer@rajasoft.com.br>
2023-05-20 23:06:00 -0300

    Adicionando o Controller da marca do veículo

---
+++ BackEnd/src/TCCPosPucMinas/TCCPosPucMinas.API/Controllers/MarcaController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using TCCPosPucMinas.Application.Interface;
+using TCCPosPucMinas.Domain.Models;
+
+namespace TCCPosPucMinas.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MarcaController : ControllerBase
+    {
+        private readonly IMarcaService _marcaService;
+
+        public MarcaController(IMarcaService marcaService)
+        {
+            this._marcaService = marcaService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var marcas = await _marcaService.GetAllMarcasAsync();
+                if (marcas == null)
+                {
+                    return NotFound("Nenhuma marca encontrada!");
+                }
+
+                return Ok(marcas);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar as marcas. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Marca>> GetById(int id)
+        {
+            try
+            {
+                var marca = await _marcaService.GetMarcaByIdAsync(id);
+                if (marca == null)
+                {
+                    return NotFound("Marca não encontrada!");
+                }
+
+                return Ok(marca);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar a marca com id: {id}. Erro: {ex.Message}");
+            }
+        }        
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Marca model)
+        {
+            try
+            {
+                var Marca = await _marcaService.AddMarca(model);
+                if (Marca == null)
+                {
+                    return BadRequest("Erro ao adicionar marca!");
+                }
+
+                return Ok(Marca);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar adicionar marca. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Marca model)
+        {
+            try
+            {
+                var Marca = await _marcaService.UpdateMarca(id, model);
+                if (Marca == null)
+                {
+                    return BadRequest("Erro ao atualizar marca!");
+                }
+
+                return Ok(Marca);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar atualizar a marca. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                return await _marcaService.DeleteMarca(id) ?
+                       Ok("Marca removida com sucesso!") :
+                       BadRequest("Não foi possível deletar a marca!");
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar deletar a marca. Erro: {ex.Message}");
+            }
+        }
+    }
+}
--- BackEnd/src/TCCPosPucMinas/TCCPosPucMinas.API/Controllers/VeiculoController.cs
+++ BackEnd/src/TCCPosPucMinas/TCCPosPucMinas.API/Controllers/VeiculoController.cs
@@ -71,6 +71,59 @@ namespace TCCPosPucMinas.API.Controllers
                 return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar os veículos da marca: {marca}. Erro: {ex.Message}");
             }
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Veiculo model)
+        {
+            try
+            {
+                var veiculo = await _veiculoService.AddVeiculo(model);
+                if (veiculo == null)
+                {
+                    return BadRequest("Erro ao adicionar veículo!");
+                }
+
+                return Ok(veiculo);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar adicionar o veículo. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Veiculo model)
+        {
+            try
+            {
+                var veiculo = await _veiculoService.UpdateVeiculo(id, model);
+                if (veiculo == null)
+                {
+                    return BadRequest("Erro ao atualizar veículo!");
+                }
+
+                return Ok(veiculo);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar atualizar o veículo. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                return await _veiculoService.DeleteVeiculo(id) ?
+                       Ok("Veículo removido com sucesso!") :
+                       BadRequest("Não foi possível deletar o veículo!");                
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar deletar veículo. Erro: {ex.Message}");
+            }
+        }
     }
 
 }
